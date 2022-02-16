bc2bc0d189b2c5e9cad404e859dcd7a691d6acf3
Bastian Ilso <hougaard.junior@gmail.com>
2022-02-16 12:07:28 +0100

    LoggingManager 2022.01: Refactor and Improve
    LoggingManager is now divided into LogStore,
    WriteToCSV and ConnectToMySQL classes.
    Furthermore, logging is significantly faster
    due to multi-threading and has a new mode
    which enables logging on the fly.

--- Assets/LoggingManager/LoggingExample.cs
+++ Assets/LoggingManager/LoggingExample.cs
@@ -17,8 +17,7 @@ public class LoggingExample : MonoBehaviour
         // Find the logging Manager in the scene.
         loggingManager = GameObject.Find("Logging").GetComponent<LoggingManager>();
 
-        // Tell the logging manager to store a piece of data into a column called "CoolData".
-        // For SQL connections, make sure that the label matches the label in your JSON file.
+        // Tell the logging manager to store a piece of data into a column called "highscore".
         loggingManager.Log("MyLabel", "Highscore", highscore);
 
         // You can also send a dictionary with multiple data entries at once.
@@ -30,11 +29,9 @@ public class LoggingExample : MonoBehaviour
         loggingManager.Log("MyLabel", otherData);
 
         // Tell the logging manager to save the data (to disk and SQL by default).
-        loggingManager.SaveLog("MyLabel");
-
-        // After saving the data, you can tell the logging manager to clear its logs.
-        // Now its ready to save more data. Saving data will append to the existing log.
-        loggingManager.ClearLog("MyLabel");
+        // Saving the data is an asynchronous process. 
+        // If you wish to clear the logs after saving, specify clear:true.
+        loggingManager.SaveLog("MyLabel", clear:true);
 
         // If you want to start a new file, you can ask loggingManager to generate
         // a new file timestamp. Saving data hereafter will go to the new file.
